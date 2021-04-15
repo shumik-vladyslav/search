f428edae3d2e35622ae5a95da16caa084b54f527
vlad.shumik <vlad.shumik@akumyn.com.au>
2021-04-15 20:47:16 +0300

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import * as XLSX from 'xlsx';
 import xlsxParser from 'xlsx-parse-json';
 @Component({
   selector: 'app-root',
@@ -8,42 +7,34 @@ import xlsxParser from 'xlsx-parse-json';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'search';
+  data;
+  dataStructure = {};
+  showDescriptionFlag;
+  selected;
   constructor(private http: HttpClient){
     this.http.get('/assets/list.xlsx', { responseType: 'blob' })
     .subscribe(res => {
       xlsxParser
       .onFileSelection(res)
       .then(data => {
-        var parsedData = data;
-        console.log(data);
-        
+        this.data = data.Sheet1;
+        console.log(this.data);
+        this.data.forEach((element, index) => {
+          if(!this.dataStructure[element.Letter]) {
+            this.dataStructure[element.Letter] = []
+          }
+          element["index"] = index;
+          this.dataStructure[element.Letter].push(element);
+        });
       });
-      // const reader = new FileReader();
-      // reader.onloadend = () => {
-      //   var base64data = reader.result;                
-      //       console.log(base64data);
-      // }
-
-      // reader.readAsDataURL(res); 
-      // console.log(res);
     });
-  
-  //   this.http.get('assets/list.xlsx', {responseType: 'text'})
-  // .subscribe((data:any) => {
-  //   console.log(data);
-     
-  // },
-  // (err) => {
-  //   if (err.error instanceof Error) {
-  //     // A client-side or network error occurred. Handle it accordingly.
-  //     console.log('An error occurred:', err.error.message);
-  //   } else {
-  //     // The backend returned an unsuccessful response code.
-  //     // The response body may contain clues as to what went wrong,
-  //     console.log(`Backend returned code ${err.status}, body was: ${err.error}`);
-  //    }
-  //   }
-  // );
+  }
+
+  showDescription(key){
+    this.selected = key;
+    this.showDescriptionFlag = true;
+  }
+  hideDescription(){
+    this.showDescriptionFlag = false;
   }
 }
